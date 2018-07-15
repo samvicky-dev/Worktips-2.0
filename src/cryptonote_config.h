eee49c3a4083d98df08fae50076f7c4939ab09aa
samvicky <vignesh260397@gmail.com>
2018-07-15 14:21:38 +0530

    Adding compatibility for Bytecoin blocks

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -45,6 +45,10 @@
 #define CURRENT_TRANSACTION_VERSION                     2
 #define CURRENT_BLOCK_MAJOR_VERSION                     1
 #define CURRENT_BLOCK_MINOR_VERSION                     1
+#define BLOCK_MAJOR_VERSION_1                           1 
+#define BLOCK_MAJOR_VERSION_2                           2 
+#define BLOCK_MAJOR_VERSION_3                           3 
+#define BLOCK_MAJOR_VERSION_4                           4 
 #define CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT_V2           300*2
 #define CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT_V3           100*3
 #define CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT_V4		CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT_V3
